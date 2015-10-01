05c806e8e1c4bbe28d3ce7ebbc24eb076cf45a86
Stefan Ravn van Overeem <stefan@stefanrvo.dk>
2015-10-01 09:05:40 +0200

    Modified Miniproject1 a bit.

--- MiniProject1/headers/PathFinder.hpp
+++ MiniProject1/headers/PathFinder.hpp
@@ -15,6 +15,8 @@ class PathFinder
     std::vector< std::vector< mapSpace > > &map;
     long size_x;
     long size_y;
+    inline void check_point(const point &p, const point &pos,
+        double &closestDistance, point &closestpoint) const;
   public:
     PathFinder(std::vector< std::vector< mapSpace > > &_map);
     virtual ~PathFinder();
--- MiniProject1/sources/PathFinder.cpp
+++ MiniProject1/sources/PathFinder.cpp
@@ -13,20 +13,19 @@ PathFinder::~PathFinder()
 }
 
 point PathFinder::FindClosestObstacle(const point &pos) const
-{
-  //Search around the position and find the closest obstacle.
-  std::vector<point> searchedpoints;
+{   //Search around the position and find the closest obstacle.
+
   point closestpoint = {9999999, 9999999};
   double closestDistance = std::numeric_limits<double>::infinity();
   int searchdistance = 1;
   while(searchdistance < pos.GetDistance(closestpoint))
-  {   //Search around the position until we find an obstacle
+  {   //Search around the position in a square spiral until we find an obstacle
     //first, find min and max searchdistances, so we don't search outside the map.
     int min_x = - searchdistance;
     if(min_x + pos.x < 0) min_x = -pos.x;
     int max_x = searchdistance;
     if( max_x + pos.x >= size_x) max_x = size_x - pos.x - 1;
-    int min_y = - searchdistance;
+    int min_y = -searchdistance;
     if(min_y + pos.y < 0) min_y = -pos.y;
     int max_y = searchdistance;
     if( max_y + pos.y >= size_y) max_y = size_y - pos.y - 1;
@@ -36,14 +35,7 @@ point PathFinder::FindClosestObstacle(const point &pos) const
     {
       for(int i = min_x; i <= max_x; i++)
       {
-        point p = pos + (point){i, searchdistance};
-        searchedpoints.push_back(p);
-        double p_distance = pos.GetDistance(p);
-        if(map[p.x][p.y] == mapSpace::obstacle && p_distance < closestDistance)
-        {
-          closestDistance = p_distance;
-          closestpoint = p;
-        }
+        check_point(pos + (point){i, searchdistance}, pos, closestDistance, closestpoint);
       }
     }
     //lower points
@@ -51,14 +43,7 @@ point PathFinder::FindClosestObstacle(const point &pos) const
     {
       for(int i = min_x; i <= max_x; i++)
       {
-        point p = pos + (point){i, -searchdistance};
-        searchedpoints.push_back(p);
-        double p_distance = pos.GetDistance(p);
-        if(map[p.x][p.y] == mapSpace::obstacle && p_distance < closestDistance)
-        {
-          closestDistance = p_distance;
-          closestpoint = p;
-        }
+        check_point(pos + (point){i, -searchdistance}, pos, closestDistance, closestpoint);
       }
     }
     //left points
@@ -66,14 +51,7 @@ point PathFinder::FindClosestObstacle(const point &pos) const
     { //add 1 to initial and substract 1 from end, so we don't check corners twice
       for(int i = min_y + 1; i <= max_y - 1; i++)
       {
-        point p = pos + (point){-searchdistance, i};
-        searchedpoints.push_back(p);
-        double p_distance = pos.GetDistance(p);
-        if(map[p.x][p.y] == mapSpace::obstacle && p_distance < closestDistance)
-        {
-          closestDistance = p_distance;
-          closestpoint = p;
-        }
+        check_point(pos + (point){-searchdistance, i}, pos, closestDistance, closestpoint);
       }
     }
     //right points
@@ -81,17 +59,21 @@ point PathFinder::FindClosestObstacle(const point &pos) const
     { //add 1 to initial and substract 1 from end, so we don't check corners twice
       for(int i = min_y + 1; i <= max_y - 1; i++)
       {
-        point p = pos + (point){searchdistance, i};
-        searchedpoints.push_back(p);
-        double p_distance = pos.GetDistance(p);
-        if(map[p.x][p.y] == mapSpace::obstacle && p_distance < closestDistance)
-        {
-          closestDistance = p_distance;
-          closestpoint = p;
-        }
+        check_point(pos + (point){searchdistance, i}, pos, closestDistance, closestpoint);
       }
     }
     searchdistance++;
   }
   return closestpoint;
+}
+
+void PathFinder::check_point(const point &p, const point &pos,
+    double &closestDistance, point &closestpoint) const
+{
+  double p_distance = pos.GetDistance(p);
+  if(map[p.x][p.y] == mapSpace::obstacle && p_distance < closestDistance)
+  {
+    closestDistance = p_distance;
+    closestpoint = p;
+  }
 }
